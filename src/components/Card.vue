073b6236e93d8c5de01d1a4b477f1dc7ab9359c6
boussole-ceria <boussole.ceria@gmail.com>
2019-07-22 22:34:11 +0200

    poll card style (justify) add icons for actions

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,50 +10,59 @@
     <div class="flex justify-between flex-wrap px-6 py-4">
       <router-link
         v-if="users.includes(userId)"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         tag="button"
         :to="{
           name: isActive ? 'poll_show' : 'poll_result',
           params: { id: id }
         }"
-        >{{ isActive ? $t("global.vote") : $t("global.results") }}</router-link
       >
+        <i class="material-icons mr-1">
+          {{ isActive ? "how_to_vote" : "pie_chart" }}
+        </i>
+        {{ isActive ? $t("global.vote") : $t("global.results") }}
+      </router-link>
       <button
         v-if="users.includes(userId)"
-        class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         @click.prevent="$emit('onShare', id)"
       >
+        <i class="material-icons mr-1">share</i>
         {{ $t("global.share") }}
       </button>
       <button
         v-if="owner === userId"
-        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         @click.prevent="$emit('onToggleStatus', id)"
       >
+        <i class="material-icons mr-1">lock_open</i>
         {{ isActive ? $t("global.close") : $t("global.reOpen") }}
       </button>
 
       <button
         v-if="users.includes(userId) === false"
-        class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         @click.prevent="$emit('onJoin', id)"
       >
+        <i class="material-icons mr-1">how_to_reg</i>
         {{ $t("global.join") }}
       </button>
 
       <button
         v-if="users.includes(userId) === true && owner !== userId"
-        class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         @click.prevent="$emit('onLeave', id)"
       >
+        <i class="material-icons mr-1">remove_circle</i>
         {{ $t("global.leave") }}
       </button>
 
       <button
         v-if="owner === userId"
-        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full my-1"
+        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full my-1 flex items-center justify-center"
         @click.prevent="$emit('onDelete', id)"
       >
+        <i class="material-icons mr-1">delete</i>
         {{ $t("global.delete") }}
       </button>
     </div>
--- src/views/PollsList.vue
+++ src/views/PollsList.vue
@@ -15,7 +15,7 @@
           class="w-full md:w-1/2 lg:w-1/3"
         >
           <Card
-            class="md:mx-2"
+            class="md:mx-2 h-full flex flex-col justify-between"
             v-bind="poll"
             :user-id="userId"
             @onShare="copyMagicLink"

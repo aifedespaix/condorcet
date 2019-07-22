48e5e177cf20412b3e1037c23469cd951a2327ab
boussole-ceria <boussole.ceria@gmail.com>
2019-07-22 21:14:09 +0200

    Patch Label click in the poll creation vue for description and is public fields

--- src/views/PollCreation.vue
+++ src/views/PollCreation.vue
@@ -22,6 +22,7 @@
             >{{ $t("poll.fields.description") }}</label
           >
           <textarea
+            id="description"
             v-model="description"
             data-testid="form-description"
             class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -30,7 +31,7 @@
           <div class="md:flex md:items-center mb-6">
             <label
               class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="description"
+              for="isPublic"
               >{{ $t("poll.fields.isPublic") }}</label
             >
             <input

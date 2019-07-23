3ca81b58da115583c550e8a222861993edceb938
jean-smaug <maximeblanc.dev@gmail.com>
2019-07-23 13:18:18 +0200

    wip

---
+++ scripts/verifyPullRequest.js
@@ -0,0 +1,33 @@
+const graphql = require('@octokit/graphql').defaults({
+    headers: {
+        authorization: `token ${process.env.GITHUB_PERSONAL_TOKEN}`
+      }
+})
+
+async function main() {
+    const { id, baseRefName } = await graphql(`
+        {
+  repository(owner: "jean-smaug", name: "condorcet") {
+    pullRequest(number: 111) {
+      id
+      baseRefName
+    }
+  }
+}
+`)
+
+    if(baseRefName !== "dev") {
+        await graphql(`
+        mutation {
+            addComment(input: {
+                subjectId: "${id}",
+                body: "salut les gens \n fezfz"
+              }) {
+                clientMutationId
+              }
+}
+`)
+    }
+}
+
+main()
